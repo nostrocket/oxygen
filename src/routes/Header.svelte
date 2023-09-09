c19ea4f706729234eef0238a1390f18ca7e455a4
qspang <26760878+shaibearary@users.noreply.github.com>
2023-09-09 19:23:14 +0800

    problem: unecessary files block npm build

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -23,9 +23,6 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="{base}/about">Something</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Dumb Game</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
