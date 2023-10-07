84508f089775eb2d78befb5bed986a59ce5df06f
gsovereignty <gsovereignty@proton.me>
2023-10-07 11:39:25 +0800

    problem: can't view events

---
+++ src/routes/eventviewer/[id]/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import ndk from "$lib/stores/ndk";
+  import { eventsInState, mempool } from "$lib/stores/state";
+  import { NDKEvent } from "@nostr-dev-kit/ndk";
+  import { CodeSnippet, InlineLoading, Tile } from "carbon-components-svelte";
+  import { Code } from "carbon-pictograms-svelte";
+
+  let event = new NDKEvent($ndk);;
+  let haveEvent = false;
+
+  $: {
+   let  m = $mempool.get($page.params.id)
+   let  s = $eventsInState.get($page.params.id)
+
+   if (m) {
+    event = m
+   }
+
+   if (s) {
+    event = s
+   }
+
+    // if (!haveEvent) {
+    //     if (mempool.fetch($page.params.id)) {
+    //     event = mempool.fetch($page.params.id)
+    // } else if (eventsInState.fetch($page.params.id)) {
+    //     event = eventsInState.fetch($page.params.id)
+    // }
+    // }
+
+    // if (event !== undefined) {
+    //     haveEvent = true
+    // }
+  }
+</script>
+{#if event.id.length == 64} 
+<Tile>
+    <CodeSnippet
+    type="multi"
+    code={JSON.stringify(event.rawEvent(),
+      null,
+      "\t"
+    )}
+    expanded
+    />
+    </Tile>
+{:else}
+<InlineLoading />
+{/if}
