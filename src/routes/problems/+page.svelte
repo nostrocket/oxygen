f00e5aa29f3dc6beb0283b9f19d8ae3d32e7ed02
gsovereignty <gsovereignty@proton.me>
2023-10-24 15:32:30 +0800

    problem: can't figure out why filter is removing some problems

--- src/routes/problems/+page.svelte
+++ src/routes/problems/+page.svelte
@@ -2,18 +2,23 @@
     import {Column, Row, TextInput} from "carbon-components-svelte";
     import AddProblem from "../../components/modals/AddProblem.svelte";
     import {consensusTipState} from "$lib/stores/nostrocket_state/master_state";
-    import {derived, writable} from "svelte/store";
+    import {derived, writable, type Readable} from "svelte/store";
     import ProblemList from "../../components/elements/problems/ProblemList.svelte";
+  import type { Problem } from "$lib/stores/nostrocket_state/types";
     //problem: cannot filter the problem tracker by content etc
     //problem: problems without titles are still being rendered becasue the ProblemComponent is pulling them from consensusTipState and our filtering is currently done in this if statement.
     //solutoin to both of these problems: create a new derived store here and pass it to the ProblemComponent so that we can filter based on user input. See https://github.com/pablof7z/vendata.io/ for examples
 
-    const queryInput = writable<string>('')
-    const problems = derived([consensusTipState, queryInput], ([$current, $queryInput]) => {
+    let problems: Readable<Map<string, Problem>>;
+    let queryInput = writable<string>('')
+
+    $: {
+        problems = derived([consensusTipState, queryInput], ([$current, $queryInput]) => {
         const filterQuery = $queryInput.toLowerCase()
         let problemArray = [...($current.Problems)]
 
         if (Boolean(filterQuery)) {
+            console.log(21)
             problemArray = [...problemArray].filter(([_, {Title, Summary, FullText}]) => {
                 const filterText = `${Title} ${Summary} ${FullText}`.toLowerCase()
                 return filterText.includes(filterQuery)
@@ -22,7 +27,15 @@
 
         return new Map(problemArray.filter(([_, {Title}]) => Boolean(Title)))
     })
+    }
 
+    $: {
+        consensusTipState.subscribe(cts=>{
+            if (cts.Problems.has("321e1ead277a2782f00460d7a0d811de4a866fd16ddc8f4d1df79e9eda78c7c2")) {
+                console.log("problem 24 Oct found. Is it rendered?")
+            }
+        })
+    }
     const handleQueryInput = (event) => $queryInput = event.detail
 </script>
 
@@ -39,7 +52,7 @@
 
 <Row padding>
     <Column>
-        <TextInput placeholder="Search..."
+        <TextInput placeholder="Filter..."
                    bind:value={$queryInput}
                    on:input={handleQueryInput}
         />
