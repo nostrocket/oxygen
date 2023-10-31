99aa94a2acfffe0f519384c3998f0df207a67bf5
Samuel Ralak <thesamuelralak@gmail.com>
2023-11-01 02:36:15 +0300

    Problem: Problem page has no proper style
    Solution:
    - Add more structure and style to the problem detail page
    - Eliminate full text from the problems list page
    - Add problem action buttons

--- src/routes/problems/[id]/+page.svelte
+++ src/routes/problems/[id]/+page.svelte
@@ -2,10 +2,12 @@
     import {consensusTipState} from "$lib/stores/nostrocket_state/master_state";
     import type {Problem} from "$lib/stores/nostrocket_state/types";
     import {page} from "$app/stores";
-    import {Column, Row, SkeletonText, Tag} from "carbon-components-svelte";
+    import {Column, OverflowMenu, OverflowMenuItem, Row, SkeletonText, Tag, Tile} from "carbon-components-svelte";
+    import {Chat, Unlocked, ManageProtection    } from "carbon-icons-svelte";
     import {ndk} from "$lib/stores/event_sources/relays/ndk";
     import type {NDKUserProfile} from "@nostr-dev-kit/ndk";
     import {makeHtml} from "$lib/helpers/mundane";
+    import AddProblemModal from "../../../components/problems/AddProblemModal.svelte";
 
     let problem: Problem
     let createdBy: NDKUserProfile | undefined
@@ -13,6 +15,7 @@
 
     $: {
         problem = $consensusTipState.Problems.get($page.params.id)
+        console.log({problem})
     }
 
     $: if (Boolean(problem?.CreatedBy) && !createdBy) {
@@ -36,46 +39,104 @@
 
 {#if Boolean(problem)}
     <Row>
-        <Column sm={12} md={8} lg={9}>
+        <Column sm={12} md={9} lg={9} class="problem-content">
             <Row>
                 <Column>
-                    <h3 style="text-transform: capitalize">
-                        {problem?.Title}
-                        <Tag size="sm" type={problem?.Closed ? 'red' : 'green'}>
-                            {problem?.Closed ? 'closed' : 'open'}
-                        </Tag>
-                    </h3>
+                    <Row>
+                        <Column md={4}>
+                            <h3 style="text-transform: capitalize">
+                                {problem?.Title}
+                            </h3>
+                            <p style="color: #94a3b8">Logged by <span style="color: #fb923c">{createdBy?.name}</span></p>
+                        </Column>
+                        <Column md={4} class="problem-item-actions">
+                            <AddProblemModal parent={problem.UID}/>
+                            <OverflowMenu size="sm" flipped light icon={ManageProtection} style="border: 2px solid white; margin-left: 1rem">
+                                <OverflowMenuItem text="Edit" hasDivider />
+                                <OverflowMenuItem text="Tag" hasDivider />
+                                <OverflowMenuItem text="Print" hasDivider />
+                                <OverflowMenuItem text="Share" hasDivider />
+                            </OverflowMenu>
+                        </Column>
+                    </Row>
 
-                    <p>{problem?.Summary || ''}</p>
+
+                </Column>
+            </Row>
+
+            <Row padding={20}>
+                <Column>
+                    <Tile>
+                        <h5 style="padding-bottom: 15px">Summary</h5>
+                        {problem?.Summary || ''}
+                    </Tile>
                 </Column>
             </Row>
 
             <Row padding={10}>
-                <Column>{@html makeHtml(problem?.FullText)}</Column>
+                <Column style="text-align: justify; text-justify: inter-word">{@html makeHtml(problem?.FullText)}</Column>
             </Row>
         </Column>
 
-        <Column md={2} lg={3}>
+        <Column md={2} lg={3} class="problem-sidebar">
             <Row>
-                <Column><p>Status</p></Column>
-                <Column><p>{problem?.Status}</p></Column>
+                <Column style="padding-bottom: 5px">
+                    <p style="display: flex; align-items: center; text-transform: capitalize">
+                        <Unlocked style={`color: ${problem?.Closed ? 'red' : 'green'}`}/>
+                        &nbsp;&nbsp;
+                        <span style={`color: ${problem?.Closed ? 'red' : 'green'}`}>{problem?.Closed ? 'Closed' : 'Open'}</span>
+                    </p>
+                </Column>
             </Row>
             <Row>
-                <Column><p>Created by</p></Column>
-                <Column><p>{createdBy?.name}</p></Column>
+                <Column style="padding-bottom: 5px">
+                    <p style="display: flex; align-items: center; text-transform: capitalize">
+                        <Chat/>&nbsp;&nbsp;{problem?.Children?.size ?? 0} Sub problems
+                    </p>
+                </Column>
             </Row>
+
+            <Row padding={5}>
+                <Column>
+                    <div style="border-bottom: 1px solid #262626; height: 5px"></div>
+                </Column>
+            </Row>
+
             <Row>
-                <Column><p>Claimed by</p></Column>
-                <Column><p>{claimedBy?.name || ''}</p></Column>
+                <Column><h5>Assignees</h5></Column>
+            </Row>
+
+            <Row padding>
+                <Column>
+                    {#if claimedBy?.name}
+                        <p>{claimedBy?.name || ''}</p>
+                    {:else}
+                        <a on:click={() => {}}>+ Claim Problem</a>
+                    {/if}
+                </Column>
+            </Row>
+
+            <Row padding>
+                <Column><h5>Status</h5></Column>
+            </Row>
+
+            <Row>
+                <Column>
+                    <Tag>{problem?.Status}</Tag>
+                </Column>
             </Row>
         </Column>
     </Row>
 {:else}
     <Row padding={10}>
-        <Column><SkeletonText heading /></Column>
+        <Column>
+            <SkeletonText heading/>
+        </Column>
     </Row>
     <Row>
-        <Column><SkeletonText paragraph /></Column>
+        <Column>
+            <SkeletonText paragraph/>
+        </Column>
     </Row>
 {/if}
 
