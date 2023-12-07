b9235e5c4ba58d5a206fb641a47251e75437a203
gsovereignty <gsovereignty@proton.me>
2023-12-07 11:26:07 +0800

    problem: can't change which rocket a problem belongs to

--- src/routes/problems/[id]/+page.svelte
+++ src/routes/problems/[id]/+page.svelte
@@ -3,13 +3,16 @@
   import { makeHtml } from "$lib/helpers/mundane";
   import { consensusTipState } from "$lib/stores/nostrocket_state/master_state";
   import { hasOpenChildren } from "$lib/stores/nostrocket_state/soft_state/simplifiedProblems";
-    import type { Problem } from "$lib/stores/nostrocket_state/types";
+  import type { Problem } from "$lib/stores/nostrocket_state/types";
   import {
     Breadcrumb,
     Button,
     ButtonSet,
     Column,
     Row,
+    Select,
+    SelectItem,
+    SelectItemGroup,
     SkeletonText,
     TextArea,
     TextInput,
@@ -32,7 +35,7 @@
 
   let canEdit = false;
   let edit = false;
-  let backupProblem:Problem;
+  let backupProblem: Problem;
 
   $: {
     if ($currentUser && problem) {
@@ -43,9 +46,12 @@
       ) {
         currentUserIsMaintainer = true;
       }
-      if (problem?.CreatedBy == $currentUser?.pubkey || currentUserIsMaintainer) {
-      canEdit = true
-    }
+      if (
+        problem?.CreatedBy == $currentUser?.pubkey ||
+        currentUserIsMaintainer
+      ) {
+        canEdit = true;
+      }
     }
   }
 
@@ -79,60 +85,98 @@
 
 {#if problem}
   <Row>
-    <Column sm={12} md={5} lg={9} class="problem-content" >
-      <div style={edit?"padding:6px;border:solid;border-width:thin;border-color:DodgerBlue;":""}>
-      <Row>
-        <Column>
-          {#if $size === "sm"}
+    <Column sm={12} md={5} lg={9} class="problem-content">
+      <div
+        style={edit
+          ? "padding:6px;border:solid;border-width:thin;border-color:DodgerBlue;"
+          : ""}
+      >
+        <Row>
+          <Column>
+            {#if $size === "sm"}
+              <Row>
+                <Column style="padding-bottom: 5px">
+                  <ProblemStatusContainer status={problemStatus(problem)} />
+                </Column>
+              </Row>
+            {/if}
             <Row>
-              <Column style="padding-bottom: 5px">
-                <ProblemStatusContainer status={problemStatus(problem)} />
+              <Column>
+                <h4 style="text-transform: capitalize">
+                  {problem?.Title}
+                  {#if canEdit}<a
+                      href="#"
+                      on:click={() => {
+                        edit = true;
+                      }}><Edit /></a
+                    >{/if}
+                </h4>
+                {#if edit}<TextInput
+                    bind:value={problem.Title}
+                    size="sm"
+                  />{/if}
               </Column>
             </Row>
-          {/if}
-          <Row>
-            <Column>
-              <h4 style="text-transform: capitalize">
-                {problem?.Title} 
-                {#if canEdit}<a href="#" on:click={()=>{
-                  edit = true;
-                }}><Edit /></a>{/if}
-              </h4>
-              {#if edit}<TextInput bind:value={problem.Title} size="sm"></TextInput>{/if}
-            </Column>
+          </Column>
+        </Row>
+
+        <Row padding>
+          <Column>
+            <Tile>
+              <h5 style="padding-bottom: 15px">Summary</h5>
+              <p>{problem?.Summary || ""}</p>
+              {#if edit}<Tile light
+                  ><TextArea bind:value={problem.Summary} /></Tile
+                >{/if}
+            </Tile>
+          </Column>
+        </Row>
+
+        <Row padding>
+          <Column>{@html makeHtml(problem?.FullText)}</Column>
+        </Row>
+        {#if edit}
+          <Row padding>
+            <Column
+              ><Tile light><TextArea bind:value={problem.FullText} /></Tile
+              ></Column
+            >
           </Row>
-        </Column>
-      </Row>
+        {/if}
 
-      <Row padding>
-        <Column>
-          <Tile>
-            <h5 style="padding-bottom: 15px">Summary</h5>
-            <p>{problem?.Summary || ""}</p>
-            {#if edit}<Tile light><TextArea bind:value={problem.Summary}></TextArea></Tile>{/if}
-          </Tile>
-        </Column>
-      </Row>
-
-      <Row padding>
-        <Column>{@html makeHtml(problem?.FullText)}</Column>
-      </Row>
-      {#if edit}
-      <Row padding>
-        <Column><Tile light><TextArea bind:value={problem.FullText}></TextArea></Tile></Column>
-      </Row>
-      {/if}
-
-      {#if edit}
-      <ButtonSet>
-        <Button kind="secondary" size="field" on:click={()=>{edit = false}}>Cancel</Button>
-        <Button size="field" on:click={()=>{
-          publishProblem($consensusTipState, problem);
-          edit = false;
-          }}>Publish Change</Button>
-      </ButtonSet>
-      {/if}
-    </div>
+        {#if edit}
+          <Select
+            hideLabel
+            size="xl"
+            labelText="Status"
+            bind:selected={problem.Rocket}
+          >
+            <SelectItemGroup label="SELECT WHICH ROCKET THIS BELONGS TO">
+              {#each $consensusTipState.RocketMap as [key, r]}
+                {#if r.CreatedBy == $currentUser?.pubkey}
+                  <SelectItem value={key} text={r.Name} />
+                {/if}
+              {/each}
+            </SelectItemGroup>
+          </Select>
+          <ButtonSet>
+            <Button
+              kind="secondary"
+              size="field"
+              on:click={() => {
+                edit = false;
+              }}>Cancel</Button
+            >
+            <Button
+              size="field"
+              on:click={() => {
+                publishProblem($consensusTipState, problem);
+                edit = false;
+              }}>Publish Change</Button
+            >
+          </ButtonSet>
+        {/if}
+      </div>
 
       <Row padding>
         <Column>
