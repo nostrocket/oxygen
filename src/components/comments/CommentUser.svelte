de81c151f31ab514b8d9da5c9f42a2aaeb128f1e
gsovereignty <gsovereignty@proton.me>
2023-12-10 14:21:09 +0800

    problem: user profile is not reactive

--- src/components/comments/CommentUser.svelte
+++ src/components/comments/CommentUser.svelte
@@ -1,52 +1,65 @@
 <script lang="ts">
-    import { ndk_profiles } from "$lib/stores/event_sources/relays/profiles";
+  import { ndk_profiles } from "$lib/stores/event_sources/relays/profiles";
   import { profiles } from "$lib/stores/hot_resources/profiles";
-    import type { NDKUser } from "@nostr-dev-kit/ndk";
-    import { InlineLoading } from "carbon-components-svelte";
-    import { Launch } from "carbon-icons-svelte";
+  import type { NDKUser } from "@nostr-dev-kit/ndk";
+  import { InlineLoading } from "carbon-components-svelte";
+  import { Launch } from "carbon-icons-svelte";
+  import { writable } from "svelte/store";
 
-    export let pubkey: string;
-    export let large:boolean = false;
+  export let pubkey: string;
+  export let large: boolean = false;
 
-    let styletag = "color: #fb923c"
+  let styletag = "color: #fb923c";
 
-    let user: NDKUser;
-    let usernameToDisplay:string|undefined = undefined;
+  let user = writable<NDKUser | undefined>(undefined);
 
-    $: if (!user) {
-        if ($profiles.has(pubkey)) {
-            let existing = $profiles.get(pubkey)
-            if (existing) {
-                user = existing
-            }
+  $: {
+    $user = getUser(pubkey);
+  }
+
+  function getUser(p: string): NDKUser | undefined {
+    let user: NDKUser | undefined = undefined;
+    if (p) {
+      if ($profiles.has(p)) {
+        let existing = $profiles.get(p);
+        if (existing) {
+          user = existing;
         }
-        if (!user) {
-            (async () => {
-            user = $ndk_profiles.getUser({ hexpubkey: pubkey });
-            user.fetchProfile().then(()=>{
-                profiles.update(s=>{
-                    s.set(user.pubkey, user)
-                    return s
-                })
-//                usernameToDisplay = user.profile?.name
+      }
+      if (!user) {
+        (async () => {
+          user = $ndk_profiles.getUser({ hexpubkey: p });
+          user.fetchProfile().then(() => {
+            profiles.update((s) => {
+              if (user) {
+                s.set(user.pubkey, user);
+              }
+              return s;
             });
-        })()
-        }
+          });
+        })();
+      }
     }
+    return user;
+  }
 
-    $: {
-        if (!usernameToDisplay) {
-            if (user?.profile?.name) {
-            usernameToDisplay = user.profile.name
-        } else if (user?.profile?.displayName) {
-            usernameToDisplay = user.profile.displayName
-        }
-        if (large) {
-            styletag = "color: #fb923c; margin: 0 5px"
-        }
-        }
+  $: {
+    if (large) {
+      styletag = "color: #fb923c; margin: 0 5px";
     }
+  }
 </script>
 
-
-{#if !usernameToDisplay}<InlineLoading description="Fetching NIP05 Data"/>{:else}<span style="color: #fb923c">{usernameToDisplay}</span><a href={"https://primal.net/p/"+user.npub} target="_blank" rel="noopener noreferrer"><Launch /></a>{/if}
+{#if $user}
+  <span style="color: #fb923c">
+    {#if $user?.profile?.name}{$user?.profile
+        ?.name}{:else if $user?.profile?.displayName}{$user?.profile
+        ?.displayName}{/if}</span
+  ><a
+    href={"https://primal.net/p/" + $user?.npub}
+    target="_blank"
+    rel="noopener noreferrer"><Launch /></a
+  >
+{:else}
+  <InlineLoading description="Fetching NIP05 Data" />
+{/if}
