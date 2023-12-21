838d4af6718ac572a67eff56f3d7d296f9010f07
gsovereignty <gsovereignty@proton.me>
2023-12-21 17:39:16 +0800

    problem: obsolete code

--- src/lib/consensus/consensus_event_producer.ts
+++ src/lib/consensus/consensus_event_producer.ts
@@ -33,86 +33,86 @@ const eventHasCausedAStateChange = new Map(); //todo: use cuckoo filter instead
 
 export const HEAD = writable<string>(rootEventID); //todo: update whenever we handle or publish a consensus event
 
-// export async function startProcessingMempoolWithConsensusLead(): Promise<void> {
-  weHaveTheLead.subscribe((weHaveIt) => {
-    if (weHaveIt) {
-      console.log("we have consensus lead");
-      processAllMempool(get(consensusTipState).Copy());
-      //handle next state change event from mempool until we get one that is valid
+// // export async function startProcessingMempoolWithConsensusLead(): Promise<void> {
+//   weHaveTheLead.subscribe((weHaveIt) => {
+//     if (weHaveIt) {
+//       console.log("we have consensus lead");
+//       processAllMempool(get(consensusTipState).Copy());
+//       //handle next state change event from mempool until we get one that is valid
 
-      //create consensus event
-    }
-  });
-}
-//process all possible mempool events
-function processAllMempool(state: Nostrocket) {
-  let bitcoinTip = BitcoinTipHeight();
-  let tipState = get(consensusTipState)
-  let previous = tipState.LastConsensusEvent()
-  let success = false
-  //todo: publish a replaceable event with our current HEAD ID and height and validate that we are appending to this so that we do not publish extra consensus events
-  for (let ev of get(stateChangeEvents)) {
-    if (ev.created_at) {
-      if (unixTimeNow() - ev.created_at < MAX_STATECHANGE_EVENT_AGE) {
-        if (labelledTag(ev, "root", "e") == rootEventID)
-          changeStateMutex(ev.id).then((release) => {
-            console.log("mutex lock " + ev.id);
-            //let tipState = get(consensusTipState).Copy();
-            if (!success) {
-              if (HandleHardStateChangeRequest(ev, tipState, ConsensusMode.Producer) == null) {
-                console.log(53, ev.id)
-                success = true
-                //todo:: copy current state instead, and update it with each event, then discard when consensus catches up
-                let consensusHeight: number = tipState.ConsensusEvents.length; //0 indexed so we don't need to ++
-                publishConsensusEvent(
-                  ev,
-                  previous,
-                  bitcoinTip.height,
-                  consensusHeight
-                )
-                  .then((e) => {
-                    console.log("consensus event created");
-                  })
-                  .catch((err) => console.log(err));
-              }
-            }
-            release();
-          });
-      }
-    }
-  }
-}
+//       //create consensus event
+//     }
+//   });
 
-async function publishConsensusEvent(
-  event: NDKEvent,
-  head: string,
-  bitcoinHeight: number,
-  consensusHeight: number
-): Promise<NDKEvent> {
-  let p = new Promise<NDKEvent>((resolve, reject) => {
-    let e = makeEvent({ kind: 15172008 });
-    e.tags.push(["e", event.id, "", "request"]);
-    e.tags.push(["event", JSON.stringify(event.rawEvent())])
-    e.tags.push(["e", head, "", "previous"]);
-    if (!simulateEvents) {
-      e.publish()
-        .then((x) => {
-          console.log("published to:", x);
-          resolve(e);
-        })
-        .catch(() => {
-          console.log("failed to publish");
-          reject("failed to publish");
-        });
-    } else {
-      e.sign().then(() => {
-        console.log("simulation mode, not publishing");
-        console.log(e.rawEvent());
-        resolve(e);
-      });
-    }
-  });
-  return p;
-}
+// //process all possible mempool events
+// function processAllMempool(state: Nostrocket) {
+//   let bitcoinTip = BitcoinTipHeight();
+//   let tipState = get(consensusTipState)
+//   let previous = tipState.LastConsensusEvent()
+//   let success = false
+//   //todo: publish a replaceable event with our current HEAD ID and height and validate that we are appending to this so that we do not publish extra consensus events
+//   for (let ev of get(stateChangeEvents)) {
+//     if (ev.created_at) {
+//       if (unixTimeNow() - ev.created_at < MAX_STATECHANGE_EVENT_AGE) {
+//         if (labelledTag(ev, "root", "e") == rootEventID)
+//           changeStateMutex(ev.id).then((release) => {
+//             console.log("mutex lock " + ev.id);
+//             //let tipState = get(consensusTipState).Copy();
+//             if (!success) {
+//               if (HandleHardStateChangeRequest(ev, tipState, ConsensusMode.Producer) == null) {
+//                 console.log(53, ev.id)
+//                 success = true
+//                 //todo:: copy current state instead, and update it with each event, then discard when consensus catches up
+//                 let consensusHeight: number = tipState.ConsensusEvents.length; //0 indexed so we don't need to ++
+//                 publishConsensusEvent(
+//                   ev,
+//                   previous,
+//                   bitcoinTip.height,
+//                   consensusHeight
+//                 )
+//                   .then((e) => {
+//                     console.log("consensus event created");
+//                   })
+//                   .catch((err) => console.log(err));
+//               }
+//             }
+//             release();
+//           });
+//       }
+//     }
+//   }
+// }
 
-//watch mempool and process each event as it comes in, if we have the lead
+// async function publishConsensusEvent(
+//   event: NDKEvent,
+//   head: string,
+//   bitcoinHeight: number,
+//   consensusHeight: number
+// ): Promise<NDKEvent> {
+//   let p = new Promise<NDKEvent>((resolve, reject) => {
+//     let e = makeEvent({ kind: 15172008 });
+//     e.tags.push(["e", event.id, "", "request"]);
+//     e.tags.push(["event", JSON.stringify(event.rawEvent())])
+//     e.tags.push(["e", head, "", "previous"]);
+//     if (!simulateEvents) {
+//       e.publish()
+//         .then((x) => {
+//           console.log("published to:", x);
+//           resolve(e);
+//         })
+//         .catch(() => {
+//           console.log("failed to publish");
+//           reject("failed to publish");
+//         });
+//     } else {
+//       e.sign().then(() => {
+//         console.log("simulation mode, not publishing");
+//         console.log(e.rawEvent());
+//         resolve(e);
+//       });
+//     }
+//   });
+//   return p;
+// }
+
+// //watch mempool and process each event as it comes in, if we have the lead
--- src/lib/protocol_validators/rockets.ts
+++ src/lib/protocol_validators/rockets.ts
@@ -1,48 +1,5 @@
 import type { Nostrocket } from "$lib/stores/nostrocket_state/types";
-import type { NDKEvent } from "@nostr-dev-kit/ndk";
-import { ignitionPubkey, nostrocketIgnitionEvent } from "../../settings";
-
-export function validate(
-  e: NDKEvent,
-  state?: Nostrocket,
-  kind?: number
-): boolean {
-  if (kind) {
-    if (e.kind != kind) {
-      return false;
-    }
-  }
-  switch (e.kind) {
-    case 1031:
-    case 15171031:
-      return validate15171031(e, state!);
-    case 15172008:
-      return validate15172008(e, state!);
-    case 31009:
-      return validate31009(e, state!);
-  }
-  return false;
-}
-
-function validate31009(e: NDKEvent, state: Nostrocket): boolean {
-  if (e.kind == 31009) {
-    if (validateIdentity(e.pubkey, state)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function validate15171031(e: NDKEvent, state: Nostrocket): boolean {
-  return true
-  if (e.kind == 15171031) {
-    //check that signer is in identity tree
-    if (validateIdentity(e.pubkey, state)) {
-      return true;
-    }
-  }
-  return false;
-}
+import { ignitionPubkey } from "../../settings";
 
 export function pubkeyHasVotepower(pubkey:string, state:Nostrocket):boolean {
         //validate signer has votepower
@@ -51,41 +8,4 @@ export function pubkeyHasVotepower(pubkey:string, state:Nostrocket):boolean {
           return true;
         }
         return false
-}
-
-function validate15172008(e: NDKEvent, state: Nostrocket): boolean {
-  if (e.kind == 15172008) {
-    //check that signer is in identity tree
-    if (validateIdentity(e.pubkey, state)) {
-      //validate signer has votepower
-      if (e.pubkey == ignitionPubkey) {
-        //todo: get pubkey's votepower from current state instead of hardcoding the ignition account
-        return true;
-      }
-      return false;
-    }
-  }
-  return false;
-}
-
-export function validateIdentity(
-  pubkey: string | undefined,
-  state?: Nostrocket
-): boolean {
-  let success = false;
-  // if (!state) {
-  //   state = get(consensusTipState);
-  // }
-  // let rocket = state.RocketMap.get(nostrocketIgnitionEvent)
-  // if (!rocket) {console.log(71)}
-  return true
-  if (pubkey) {
-    if (
-      pubkey == ignitionPubkey ||
-      state!.RocketMap.get(nostrocketIgnitionEvent)?.isParticipant(pubkey!)
-    ) {
-      success = true;
-    }
-  }
-  return success;
 }
