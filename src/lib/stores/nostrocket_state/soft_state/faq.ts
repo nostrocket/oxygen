76d3e82a24872e7f52bcb23538c7ab5498c23b8b
gsovereignty <gsovereignty@proton.me>
2024-01-01 22:59:12 +0000

    problem: updates to FAQs are not being handled

--- src/lib/stores/nostrocket_state/soft_state/faq.ts
+++ src/lib/stores/nostrocket_state/soft_state/faq.ts
@@ -3,58 +3,85 @@ import type { NDKEvent } from "@nostr-dev-kit/ndk";
 import { nostrocketIgnitionEvent } from "../../../../settings";
 import { FAQ, type Nostrocket } from "../types";
 
-export function HandleFAQEvent(
-    ev: NDKEvent,
-    state: Nostrocket
-  ): Error | null {
-    if (!state.RocketMap.get(nostrocketIgnitionEvent)?.isParticipant(ev.pubkey)) {
-      return new Error("pubkey not in identity tree");
-    }
-    switch (ev.kind) {
-      case 1122:
-        if (ev.getMatchingTags("new").length > 0) {
-          return handleNewFAQ(ev, state);
-        }
-        if (labelledTag(ev, "", "modifies")) {
-          return handleModification(ev, state);
-        }
-    }
-    return new Error("invalid event kind");
+export function HandleFAQEvent(ev: NDKEvent, state: Nostrocket): Error | null {
+  if (!state.RocketMap.get(nostrocketIgnitionEvent)?.isParticipant(ev.pubkey)) {
+    return new Error("pubkey not in identity tree");
   }
+  switch (ev.kind) {
+    case 1122:
+      if (ev.getMatchingTags("new").length > 0) {
+        return handleNewFAQ(ev, state);
+      }
+      if (labelledTag(ev, "", "modifies")) {
+        return handleModification(ev, state);
+      }
+  }
+  return new Error("invalid event kind");
+}
 
 function handleNewFAQ(ev: NDKEvent, state: Nostrocket): Error | null {
-    let f = new FAQ()
-    let question = labelledTag(ev, "question", "text")
-    if (!question) {
-        return new Error("could not find question")
-    }
-    f.Question = question
-    let [rocket, err] = getRocket(ev, state)
-    if (err != null) {
-        return err
-    }
-    if (!rocket) {throw new Error("this should not happen")}
-    f.RocketID = rocket.UID
-    let sentence = labelledTag(ev, "sentence", "text")
-    let paragraph = labelledTag(ev, "paragraph", "text")
-    let page = labelledTag(ev, "page", "text")
-    if (sentence) {
-        f.AnswerSentence = sentence
-    }
-    if (paragraph) {
-        f.AnswerParagraph = paragraph
-    }
-    if (page) {
-        f.AnswerPage = page
-    }
-    f.CreatedBy = ev.pubkey
-    f.LastUpdateUnix = ev.created_at!
+  let f = new FAQ();
 
-    f.Events.push(ev.id)
-    f.UID = ev.id;
-    rocket.FAQ.set(f.UID, f)
-    return null
+  let [rocket, err] = getRocket(ev, state);
+  if (err != null) {
+    return err;
+  }
+  if (!rocket) {
+    throw new Error("this should not happen");
+  }
+  f.RocketID = rocket.UID;
+  err = populateFAQFromEvent(f, ev)
+  if (err != null) {return err}
+  f.CreatedBy = ev.pubkey;
+  f.LastUpdateUnix = ev.created_at!;
+  f.Events.push(ev.id);
+  f.UID = ev.id;
+  rocket.FAQ.set(f.UID, f);
+  return null;
 }
 function handleModification(ev: NDKEvent, state: Nostrocket): Error | null {
-    return new Error("Function not implemented.");
+  let [rocket, err] = getRocket(ev, state);
+  if (err != null) {
+    return err;
+  }
+  if (!rocket) {
+    throw new Error("this should not happen");
+  }
+  let modID = labelledTag(ev, "", "modifies");
+  if (!modID) {
+    return new Error("could not find a modification ID");
+  }
+  let f = rocket.FAQ.get(modID);
+  if (!f) {
+    return new Error("could not find existing FAQ in current state");
+  }
+  let errP = populateFAQFromEvent(f, ev)
+  if (errP != null) {return err}
+  if (ev.created_at! <= f.LastUpdateUnix) {
+    return new Error ("this event was published earlier than the latest one we have")
+  }
+  f.LastUpdateUnix = ev.created_at!;
+  f.Events.push(ev.id);
+  return null
+}
+
+function populateFAQFromEvent(f: FAQ, ev: NDKEvent): Error | null {
+  let question = labelledTag(ev, "question", "text");
+  if (!question) {
+    return new Error("could not find question");
+  }
+  f.Question = question;
+  let sentence = labelledTag(ev, "sentence", "text");
+  let paragraph = labelledTag(ev, "paragraph", "text");
+  let page = labelledTag(ev, "page", "text");
+  if (sentence) {
+    f.AnswerSentence = sentence;
+  }
+  if (paragraph) {
+    f.AnswerParagraph = paragraph;
+  }
+  if (page) {
+    f.AnswerPage = page;
+  }
+  return null
 }
